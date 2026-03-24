5234f2629ef2330bf588146c6b744196fc1904e7
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-03-24 23:11:43 +0530

    add script.js (element selectors)

---
+++ script.js
@@ -0,0 +1,7 @@
+// Element Selectors - abhinavgautam08
+var taskInput = document.getElementById('taskInput');
+var addTaskBtn = document.getElementById('addTaskBtn');
+var taskList = document.getElementById('taskList');
+var statsCounter = document.getElementById('statsCounter'); 
+var productivityBar = document.getElementById('productivityBar');
+var productivityPercentage = document.getElementById('productivityPercentage');
