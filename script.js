977c71d256bfb4490e0bfcf78a21e98f7c875c3a
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-03-24 23:13:27 +0530

    tailwind config

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ var taskList = document.getElementById('taskList');
 var statsCounter = document.getElementById('statsCounter'); 
 var productivityBar = document.getElementById('productivityBar');
 var productivityPercentage = document.getElementById('productivityPercentage');
+
+// tailwind config - abhinavgautam08
+tailwind.config = {
+  darkMode: "class",
+  theme: { extend: {
+      colors: { "on-secondary-container": "#646464","surface-container": "#eeeeee","on-primary-fixed": "#410000","surface-variant": "#e2e2e2","on-error": "#ffffff","surface-tint": "#c00000","outline": "#946e68","on-tertiary-fixed": "#1b1b1b","surface-container-lowest": "#ffffff","on-surface": "#1a1c1c","surface-container-high": "#e8e8e8","error-container": "#ffdad6","primary-fixed-dim": "#ffb4a8","primary-container": "#e60000","on-error-container": "#93000a","on-secondary": "#ffffff","primary-fixed": "#ffdad4","on-tertiary-fixed-variant": "#474747","surface-container-highest": "#e2e2e2","primary": "#b70100","inverse-on-surface": "#f0f1f1","surface-dim": "#dadada","surface-bright": "#f9f9f9","on-surface-variant": "#5f3f3a","error": "#ba1a1a","secondary-fixed": "#e2e2e2","secondary-fixed-dim": "#c6c6c6","tertiary-container": "#727272","on-background": "#1a1c1c","on-primary-container": "#fff7f5","background": "#f9f9f9","outline-variant": "#e9bcb5","on-tertiary": "#ffffff","tertiary-fixed": "#e2e2e2","surface": "#f9f9f9","on-secondary-fixed": "#1b1b1b","inverse-surface": "#2f3131","inverse-primary": "#ffb4a8","tertiary": "#5a5a5a","on-tertiary-container": "#f8f8f8","secondary": "#5e5e5e","secondary-container": "#e2e2e2","on-secondary-fixed-variant": "#474747","surface-container-low": "#f3f3f4","on-primary": "#ffffff","tertiary-fixed-dim": "#c6c6c6","on-primary-fixed-variant": "#930100"
+      },
+      fontFamily: { "headline": ["Space Grotesk"],"body": ["Space Grotesk"],"label": ["Space Grotesk"]
+      },
+      borderRadius: {"DEFAULT": "0px", "lg": "0px", "xl": "0px", "full": "9999px"},
+    },
+  },
+}
